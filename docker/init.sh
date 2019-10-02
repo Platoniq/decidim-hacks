b133d91c8cdbb80ba04521e1a92bc7912a1e6b0a
Ivan Vergés <ivan@platoniq.net>
2019-10-02 20:01:19 +0200

    Init example repositori

---
+++ docker/init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+pwd
+bundle install
+echo -e "\e[33m Upgrading Decidim..."
+bin/rails decidim:upgrade
+
+echo -e "\e[33m Trying to execute migrations..."
+if bin/rails db:migrate; then
+    echo -e "\e[32m Database already created. No need for seeding."
+else
+    echo -e "\e[31m Migration failed. Installing database"
+    bin/rails db:create
+    echo -e "\e[33m Executing migrations..."
+    bin/rails db:migrate
+    echo -e "\e[32m Database just created so let's seed some data..."
+    bin/rails db:seed
+fi
+echo -e "\e[33m Starting rails server..."
+bundle exec puma
+# bundle exec rails server
